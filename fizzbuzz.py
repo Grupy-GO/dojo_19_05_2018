e0d837d212daf3341364c591bc6d0b69827c717d
Rayan Sóstenes <rayancocos@gmail.com>
2018-05-19 22:22:20 -0300

    Add alternative implementation of fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -9,8 +9,39 @@ Sample output:
 1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz
 '''
 
+
 def fizzbuzz(n: int):
-    return (n % 3 == 0) * 'Fizz' + (n % 5 == 0) * 'Buzz' + (n%3  != 0 and n%5 != 0) * str(n)
+    ret = ''
+    if n % 3 == 0:
+        ret += 'Fizz'
+    if n % 5 == 0:
+        ret += 'Buzz'
+    if n % 3 != 0 and n % 5 != 0:
+        ret = str(n)
+    return ret
+
+
+def fizzbuzz_without_if_rayan(n: int):
+    return (n % 3 == 0) * 'Fizz' + (n % 5 == 0) * 'Buzz' + (n % 3 != 0 and n % 5 != 0) * str(n)
+
+
+def fizzbuzz_without_if_nathan(n: int):
+    ''' Implementação do algoritimo sem uso de if's que ganhou a caneca '''
+
+    ret = ''
+    while (n % 3 == 0):
+        ret += 'Fizz'
+        break
+
+    while (n % 5 == 0):
+        ret += 'Buzz'
+        break
+
+    while (n % 3 != 0 and n % 5 != 0):
+        ret = str(n)
+        break
+
+    return ret
 
 
 def main():
--- fizzbuzz_test.py
+++ fizzbuzz_test.py
@@ -1,15 +1,23 @@
 import unittest
-from fizzbuzz import fizzbuzz
+from fizzbuzz import fizzbuzz, fizzbuzz_without_if_nathan, fizzbuzz_without_if_rayan
 
 class TestCase(unittest.TestCase):
     def test_fizzbuzz_1(self):
         self.assertEqual(fizzbuzz(1), '1')
+        self.assertEqual(fizzbuzz_without_if_nathan(1), '1')
+        self.assertEqual(fizzbuzz_without_if_rayan(1), '1')
 
     def test_fizzbuzz_3(self):
         self.assertEqual(fizzbuzz(3), 'Fizz')
+        self.assertEqual(fizzbuzz_without_if_nathan(3), 'Fizz')
+        self.assertEqual(fizzbuzz_without_if_rayan(3), 'Fizz')
 
     def test_fizzbuzz_5(self):
         self.assertEqual(fizzbuzz(5), 'Buzz')
+        self.assertEqual(fizzbuzz_without_if_nathan(5), 'Buzz')
+        self.assertEqual(fizzbuzz_without_if_rayan(5), 'Buzz')
     
     def test_fizzbuzz_15(self):
         self.assertEqual(fizzbuzz(15), 'FizzBuzz')
+        self.assertEqual(fizzbuzz_without_if_nathan(15), 'FizzBuzz')
+        self.assertEqual(fizzbuzz_without_if_rayan(15), 'FizzBuzz')
